7a7c98fbfce95a0bbf256e30b3eb0b42056643dd
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2025-03-11 17:44:03 +0100

    User Entity

--- backend/src/main/java/com/aziz/CampusmarketApplication.java
+++ backend/src/main/java/com/aziz/CampusmarketApplication.java
@@ -1,4 +1,4 @@
-package com.campusmarket;
+package com.aziz;
 
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
---
+++ backend/src/main/java/com/aziz/modal/Coupon.java
@@ -0,0 +1,42 @@
+package com.aziz.modal;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Coupon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO) // generate Id automatically ( no id from frontend when new user created )
+    private Long id;
+
+    private String code;
+
+    private double discountPercentage;
+
+    private LocalDate validityStartDate;
+
+    private LocalDate validityEndDate;
+
+    private double minimumOrderValue;
+
+    private boolean isActive = true;
+
+    @ManyToMany(mappedBy = "usedCoupons")
+    private Set<User> usedByUsers = new HashSet<>();
+
+
+
+
+
+
+}
