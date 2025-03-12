b26df11dbb455e4fb17af1de73568a7fdf05965d
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2025-03-12 11:38:25 +0100

    Cart Entity

---
+++ backend/src/main/java/com/aziz/modal/Cart.java
@@ -0,0 +1,38 @@
+package com.aziz.modal;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    // Each user has one cart
+    @OneToOne
+    private User user;
+
+    // mappedBy = no extra table for it + A cart can contain multiple cart items
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<CartItem> cartItems = new HashSet<>();
+
+
+    private double totalSellingPrice;
+
+    private int totalItem;
+
+    private int totalMrpPrice;
+
+    private int discount;
+
+    private String couponCode;
+}
