885e23b67303c2c5d382e7724148ee34dac9d822
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2025-03-12 12:02:34 +0100

    Seller Entity

--- backend/src/main/java/com/aziz/modal/Seller.java
+++ backend/src/main/java/com/aziz/modal/Seller.java
@@ -1,4 +1,45 @@
 package com.aziz.modal;
 
+import com.aziz.domain.AccountStatus;
+import com.aziz.domain.USER_ROLE;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 public class Seller {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String sellerName;
+
+    private String mobile;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+    private String password;
+
+    @Embedded
+    private BusinessDetails businessDetails = new BusinessDetails();
+
+    @Embedded
+    private BankDetails bankDetails = new BankDetails();
+
+    // A seller has one pickup address
+    @OneToOne(cascade = CascadeType.ALL)
+    private Address pickupaddress = new Address();
+
+    private String fiscalCode;
+
+    private USER_ROLE role = USER_ROLE.ROLE_SELLER;
+
+    private boolean isEmailVerified = false;
+
+    private AccountStatus accountStatus = AccountStatus.PENDING_VERIFICATION;
 }
