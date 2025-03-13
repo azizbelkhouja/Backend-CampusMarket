2c93d4f6f183c7f8862a96a1ae9d6b1639f7ce88
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2025-03-13 12:20:06 +0100

    Login / Signup Auth

---
+++ backend/src/main/java/com/aziz/controller/AuthController.java
@@ -0,0 +1,31 @@
+package com.aziz.controller;
+
+import com.aziz.modal.User;
+import com.aziz.repository.UserRepository;
+import com.aziz.response.SignupRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UserRepository userRepository;
+
+    @PostMapping("/signup")
+    public ResponseEntity<User> createUserHandler(@RequestBody SignupRequest req) {
+
+        User user = new User();
+        user.setEmail(req.getEmail());
+        user.setFullName(req.getFullName());
+
+        User savedUser = userRepository.save(user);
+
+        return ResponseEntity.ok(user);
+    }
+}
