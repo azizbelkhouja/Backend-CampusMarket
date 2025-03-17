b2cfefedb36f6ca815a54ad647d8b51b77d39b32
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2025-03-17 22:18:53 +0100

    refactor: optimize product creation and filtering logic

---
+++ backend/src/main/java/com/aziz/service/ProductService.java
@@ -0,0 +1,31 @@
+package com.aziz.service;
+
+import com.aziz.exceptions.ProductException;
+import com.aziz.modal.Product;
+import com.aziz.modal.Seller;
+import com.aziz.request.CreateProductRequest;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+public interface ProductService {
+
+    public Product createProduct(CreateProductRequest req, Seller seller);
+    public void deleteProduct(Long productId) throws ProductException;
+    public Product updateProduct(Long productId, Product product) throws ProductException;
+    Product findProductById(Long productId) throws ProductException;
+    List<Product> searchProducts(String query);
+    public Page<Product> getAllProducts(
+            String category,
+            String brand,
+            String colors,
+            String sizes,
+            Integer minPrice,
+            Integer maxPrice,
+            Integer minDiscount,
+            String sort,
+            String stock,
+            Integer pageNumber
+    );
+    List<Product> getProductBySellerId(Long sellerId);
+}
