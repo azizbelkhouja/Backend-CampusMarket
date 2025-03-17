243eab53439c3a558a134bfa5df44ddfc60d1db7
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2025-03-17 23:00:17 +0100

    SellerProductController

---
+++ backend/src/main/java/com/aziz/controller/SellerProductController.java
@@ -0,0 +1,69 @@
+package com.aziz.controller;
+
+
+import com.aziz.exceptions.ProductException;
+import com.aziz.modal.Product;
+import com.aziz.modal.Seller;
+import com.aziz.request.CreateProductRequest;
+import com.aziz.service.ProductService;
+import com.aziz.service.SellerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/seller/product")
+public class SellerProductController {
+
+    private final ProductService productService;
+    private final SellerService sellerService;
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getProductBySellerId(
+            @RequestHeader("Authorization") String jwt) throws Exception {
+
+        Seller seller = sellerService.getSellerProfile(jwt);
+
+        List<Product> products = productService.getProductBySellerId(seller.getId());
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    @PostMapping()
+    public ResponseEntity<Product> createProduct(
+            @RequestBody CreateProductRequest request,
+            @RequestHeader("Authorization") String jwt)
+            throws Exception {
+
+        Seller seller = sellerService.getSellerProfile(jwt);
+
+        Product product = productService.createProduct(request, seller);
+        return new ResponseEntity<>(product, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{productId}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long productId) {
+        try {
+            productService.deleteProduct(productId);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (ProductException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/{productId}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long productId,
+                                                 @RequestBody Product product) {
+        try {
+            Product updatedProduct = productService.updateProduct(productId, product);
+            return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
+        } catch (ProductException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+}
