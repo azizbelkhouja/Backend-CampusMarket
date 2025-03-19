608eba1a8d3d335eca865a5302070a643c13be11
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2025-03-19 22:59:24 +0100

    stripe payment integration

--- backend/src/main/java/com/aziz/service/PaymentService.java
+++ backend/src/main/java/com/aziz/service/PaymentService.java
@@ -1,4 +1,18 @@
 package com.aziz.service;
 
-public class PaymentService {
+import com.aziz.modal.Order;
+import com.aziz.modal.PaymentOrder;
+import com.aziz.modal.User;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentLink;
+
+import java.util.Set;
+
+public interface PaymentService {
+
+    PaymentOrder createOrder(User user, Set<Order> orders);
+    PaymentOrder getPaymentOrderById(Long orderId) throws Exception;
+    PaymentOrder getPaymentOrderByPaymentId(String paymentId) throws Exception;
+    Boolean ProceedPaymentOrder(PaymentOrder paymentOrder, String paymentId, String paymentLinkId);
+    String createStripePaymentLink(User user, Long amount, Long orderId) throws StripeException;
 }
