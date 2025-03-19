ad4fba2693114fc3b00e4c289ae5ab8cc52453cf
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2025-03-19 23:26:11 +0100

    Payment feature added 🤑
    Transactions and payments using stripe

---
+++ backend/src/main/java/com/aziz/service/impl/TransactionServiceImpl.java
@@ -0,0 +1,61 @@
+package com.aziz.service.impl;
+
+import com.aziz.modal.Order;
+import com.aziz.modal.Seller;
+import com.aziz.modal.Transaction;
+import com.aziz.repository.SellerRepository;
+import com.aziz.repository.TransactionRepository;
+import com.aziz.service.TransactionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class TransactionServiceImpl implements TransactionService{
+
+    private final TransactionRepository transactionRepository;
+    private final SellerRepository sellerRepository;
+
+    @Override
+    public Transaction createTransaction(Order order) {
+
+        Seller seller = sellerRepository.findById(order.getSellerId()).get();
+
+        Transaction transaction = new Transaction();
+        transaction.setSeller(seller);
+        transaction.setCustomer(order.getUser());
+        transaction.setOrder(order);
+
+        return transactionRepository.save(transaction);
+    }
+
+    @Override
+    public List<Transaction> getTransactionBySellerId(Seller seller) {
+
+        return transactionRepository.findBySellerId(seller.getId());
+    }
+
+    @Override
+    public List<Transaction> getAllTransactions() {
+
+        return transactionRepository.findAll();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
