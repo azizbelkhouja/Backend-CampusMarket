656da0ccd7511a4c89a39febdc18f449923018a1
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2025-03-19 22:26:38 +0100

    Add Order module

---
+++ backend/src/main/java/com/aziz/service/OrderService.java
@@ -0,0 +1,18 @@
+package com.aziz.service;
+
+import com.aziz.domain.OrderStatus;
+import com.aziz.modal.*;
+
+import java.util.List;
+import java.util.Set;
+
+public interface OrderService {
+
+    Set<Order> createOrder(User user, Address shippingAddress, Cart cart);
+    Order findOrderById(Long id) throws Exception;
+    List<Order> userOrdersHistory(Long userId);
+    List<Order> sellerOrders(Long SellerId);
+    Order updateOrderStatus(Long id, OrderStatus orderStatus) throws Exception;
+    Order cancelOrder(Long orderId, User user) throws Exception;
+    OrderItem getOrderItemById(Long id) throws Exception;
+}
---
+++ backend/src/main/java/com/aziz/service/impl/OrderServiceImpl.java
@@ -0,0 +1,151 @@
+package com.aziz.service.impl;
+
+import com.aziz.domain.OrderStatus;
+import com.aziz.domain.PaymentStatus;
+import com.aziz.modal.*;
+import com.aziz.repository.AddressRepository;
+import com.aziz.repository.OrderItemRepository;
+import com.aziz.repository.OrderRepository;
+import com.aziz.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceImpl implements OrderService {
+
+    private final OrderRepository orderRepository;
+    private final AddressRepository addressRepository;
+    private final OrderItemRepository orderItemRepository;
+
+    @Override
+    public Set<Order> createOrder(User user, Address shippingAddress, Cart cart) {
+
+        if (!user.getAddresses().contains(shippingAddress)) {
+            user.getAddresses().add(shippingAddress);
+        }
+        Address address = addressRepository.save(shippingAddress);
+
+        Map<Long, List<CartItem>> itemsBySeller = cart.getCartItems().stream()
+                .collect(Collectors.groupingBy(item->item.getProduct()
+                        .getSeller().getId()));
+        Set<Order> orders = new HashSet<>();
+
+        for (Map.Entry<Long, List<CartItem>> entry : itemsBySeller.entrySet()) {
+            Long sellerId = entry.getKey();
+            List<CartItem> items = entry.getValue();
+
+            int totalOrderPrice = items.stream().mapToInt(
+                    CartItem::getSellingPrice
+            ).sum();
+            int totalItem = items.stream().mapToInt(CartItem::getQuantity).sum();
+
+            Order createdOrder = new Order();
+            createdOrder.setUser(user);
+            createdOrder.setSellerId(sellerId);
+            createdOrder.setTotalMrpPrice(totalOrderPrice);
+            createdOrder.setTotalSellingPrice(totalOrderPrice);
+            createdOrder.setTotalItem(totalItem);
+            createdOrder.setShippingAddress(address);
+            createdOrder.setOrderStatus(OrderStatus.PENDING);
+            createdOrder.getPaymentDetails().setStatus(PaymentStatus.PENDING);
+
+            Order savedOrder = orderRepository.save(createdOrder);
+            orders.add(savedOrder);
+
+            List<OrderItem> orderItems = new ArrayList<>();
+
+            for (CartItem item : items) {
+                OrderItem orderItem = new OrderItem();
+
+                orderItem.setOrder(savedOrder);
+                orderItem.setMrpPrice(item.getMrpPrice());
+                orderItem.setProduct(item.getProduct());
+                orderItem.setQuantity(item.getQuantity());
+                orderItem.setSize(item.getSize());
+                orderItem.setUserId(item.getUserId());
+                orderItem.setSellingPrice(item.getSellingPrice());
+
+                savedOrder.getOrderItems().add(orderItem);
+
+                OrderItem savedOrderItem = orderItemRepository.save(orderItem);
+                orderItems.add(savedOrderItem);
+
+            }
+
+        }
+        return orders;
+    }
+
+    @Override
+    public Order findOrderById(Long id) throws Exception {
+        return orderRepository.findById(id).orElseThrow(
+                () -> new Exception("Order not found")
+        );
+    }
+
+    @Override
+    public List<Order> userOrdersHistory(Long userId) {
+        return orderRepository.findByUserId(userId);
+    }
+
+    @Override
+    public List<Order> sellerOrders(Long SellerId) {
+        return orderRepository.findBySellerId(SellerId);
+    }
+
+    @Override
+    public Order updateOrderStatus(Long id, OrderStatus orderStatus) throws Exception {
+
+        Order order = findOrderById(id);
+        order.setOrderStatus(orderStatus);
+
+        return orderRepository.save(order);
+    }
+
+    @Override
+    public Order cancelOrder(Long id, User user) throws Exception {
+
+        Order order = findOrderById(id);
+
+        if (user.getId().equals(order.getUser().getId())) {
+            throw new Exception("You can't access the order");
+        }
+        order.setOrderStatus(OrderStatus.CANCELLED);
+
+        return orderRepository.save(order);
+    }
+
+    @Override
+    public OrderItem getOrderItemById(Long id) throws Exception {
+        return orderItemRepository.findById(id).orElseThrow(
+                () -> new Exception("Can't access Order Item")
+        );
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
