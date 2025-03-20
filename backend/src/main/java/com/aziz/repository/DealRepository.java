3d5069d34854e09ddcc7f4d71799444d9ad39482
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2025-03-20 12:39:03 +0100

    Deal Functionality

--- backend/src/main/java/com/aziz/repository/DealRepository.java
+++ backend/src/main/java/com/aziz/repository/DealRepository.java
@@ -1,4 +1,8 @@
 package com.aziz.repository;
 
-public interface DealRepository {
+import com.aziz.modal.Deal;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DealRepository extends JpaRepository<Deal, Long> {
+
 }
