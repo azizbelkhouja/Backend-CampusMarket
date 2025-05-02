be32ac7e56e7781306473f2d09d5db91ad274bd8
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2025-05-02 20:27:33 +0200

    project initializr

---
+++ campusmarket/src/main/java/com/aziz/campusmarket/CampusmarketApplication.java
@@ -0,0 +1,13 @@
+package com.aziz.campusmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusmarketApplication.class, args);
+	}
+
+}
